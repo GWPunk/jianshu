52eedf0fdc7c20abb4f6b5df4dc6ffcebc1eb0eb
gao <1124960732@qq.com>
2021-05-22 18:28:17 +0800

    project init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./style.js";
+
+function App() {
+  return (
+    <div className="demo">
+      <GlobalStyle />
+      hello world
+    </div>
+  );
+}
+
+export default App;
