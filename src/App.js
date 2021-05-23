f19a671bde2499082849da06ab4d7576b337aacf
Govin <1124960732@qq.com>
2021-05-23 12:26:13 +0800

    index-header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { GlobalStyle } from "./style.js";
+import Header from "./common/header";
 
 function App() {
   return (
     <div className="demo">
       <GlobalStyle />
-      hello world
+      <Header/>
     </div>
   );
 }
---
+++ src/common/header/index.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {HeaderWrapper,Logo,Nav,NavItem, NavSearch,Addition,Button} from './style'
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <HeaderWrapper>
+          <Logo/>
+          <Nav>
+            <NavItem className="left active">首页</NavItem>
+            <NavItem className="left">下载App</NavItem>
+            <NavItem className="right">登录 </NavItem>
+            <NavItem className="right">Aa</NavItem>
+            <NavSearch/>
+          </Nav>
+          <Addition>
+            <Button className="write">写文章</Button>
+            <Button className="reg">注册</Button>
+          </Addition>
+        </HeaderWrapper>
+      </div>
+    )
+  }
+}
+export default Header  
